bcbdc4b3d2829c83c676e05fb5dddfe8a351894b
ZAII <ivy2846357@icloud.com>
2021-11-11 19:21:09 +0800

    開發環境修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
-//載入node.js的路徑(不需更動)
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
 //輸出內容
 module.exports = {
-    entry: './src/js/all.js', 
+    entry: './src/js/all.js',
 
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
     },
 
     devServer: {
@@ -22,7 +30,53 @@ module.exports = {
     module: {
         rules: [{
             test: /\.s[ac]ss$/i,
-            use: ["style-loader","css-loader","sass-loader",],
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", ],
+        }, {
+            test: /\.(png|jpe?g|gif|svg|webp)$/i,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: 'img/[name].[ext]',
+                    publicPath: '../',
+                },
+            }, ],
+        }, {
+            test: /\.html$/i,
+            loader: "html-loader",
+        }, {
+            loader: 'image-webpack-loader',
+            options: {
+                disable: process.env.NODE_ENV === 'production' ? false : true,
+            },
         }, ],
     },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`${path.resolve(__dirname, 'src')}/**/*`, {
+                nodir: true,
+            }),
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorPluginOptions: {
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }],
+            },
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+    ],
 };
